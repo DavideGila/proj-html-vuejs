baa62a5c523d522c7ddade320299fd408d06916f
DavideGila <davide.gila@gmail.com>
2023-11-21 13:14:52 +0100

    created navbar

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav>
+        <div class="d-flex justify-content-between align-items-center p-4">
+            <img src="/images/logo.svg" alt="Logo" id="logo">
+            <ul class="d-flex m-0">
+                <li v-for="nav in store.navList" class="text-uppercase px-4">{{ nav }}</li>
+            </ul>
+            <div class="d-flex">
+                <div>
+                    <img :src="store.navIcon[0]" alt="" class="mx-4 pe-4 border-end">
+                </div>
+                <div>
+                    <span v-for="icon in store.navIcon.slice(1)"><img :src="icon" alt="" class="px-3 icons"></span>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { store } from '../data/store'
+
+    export default {
+        data() {
+            return {
+                store
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+@use '../assets/styles/partials/variables' as *;
+
+#logo {
+    width: 200px;
+}
+
+li{
+    font-size: 13px;
+    font-weight: 700;
+}
+</style>
