8b4cd8e0585c419df98123e0761bb370f3cf27c1
DavideGila <davide.gila@gmail.com>
2023-11-21 13:14:30 +0100

    created two arrays

--- src/data/store.js
+++ src/data/store.js
@@ -1,2 +1,19 @@
 import {reactive} from 'vue';
-export const store = reactive({})
+export const store = reactive({
+    navList: [
+        'Home',
+        'Blog',
+        'Pages',
+        'About',
+        'Contacts',
+        'Gallery',
+        'Shop'
+    ],
+    navIcon: [
+        '/images/image (8).svg',
+        '/images/image (9).svg',
+        '/images/image (10).svg',
+        '/images/image (11).svg',
+        '/images/image (12).svg'
+    ]
+})
