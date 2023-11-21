f68d64ea67aaf4fd266e8de3d0bb0c549acb99df
DavideGila <davide.gila@gmail.com>
2023-11-21 13:16:00 +0100

    imported HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-
-  </div>
+  <HeaderComponent />
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue'
+
   export default {
+    components:{
+      HeaderComponent
+    }
     
   }
 </script>
 
 <style lang="scss" scoped>
-
+@use './assets/styles/partials/variables' as *;
 </style>
