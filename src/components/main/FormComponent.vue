8de5be16cdefb5b2a98c6a8cdb3120203a2601c6
DavideGila <davide.gila@gmail.com>
2023-11-23 10:55:15 +0100

    created ninth section with form

---
+++ src/components/main/FormComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <section>
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-6">
+                    <h4 class="text-uppercase mb-4">Collaborate</h4>
+                    <h2 class="text-uppercase mb-4">Want to Work With Us? Leave Us a Message!</h2>
+                    <p>Duis sit amet congue ipsum. Nulla maximus My venenatis posuere.To The Mauris ut tempus Suspendisse porta tincidunt lectus non of me pellentesque. At A Sed blandit at diam sit amet Orci varius natoque penatibus et dis dis to parturient montes, My Car nascetur ridiculus Sed feugiat lacus in erat egestas, quis eleifend lacus imperdiet.</p>
+                </div>
+
+                <!-- Form -->
+                <div class="col-6 mt-5">
+                    <div class="row">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Your name" aria-label="Your name">
+                        </div>
+                        <div class="col">
+                            <input type="email" class="form-control" placeholder="Your email" aria-label="Your email">
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Mobile number" aria-label="Mobile number">
+                        </div>
+                        <div class="col">
+                            <input type="email" class="form-control" placeholder="Your Pincode" aria-label="Your Pincode">
+                        </div>
+                    </div>
+                    <div class="mt-4">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Enter Your Message" rows="7"></textarea>
+                    </div>
+                    <button class="text-uppercase form-control mt-4 btn btn-dark">Submit now</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
